92c6c23175962a984ccbe985c37b2839ddd838cb
Yevhenii2022 <3starosta3@gmail.com>
2023-02-24 20:01:30 +0200

    add task - 9

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const loginFormEl = document.querySelector('.login-form');
 
+const user = {};
+
+loginFormEl.addEventListener('submit', handleLoginFormElSubmit);
+
+function handleLoginFormElSubmit(event) {
+	event.preventDefault();
+
+	const {
+		elements: { email, password },
+	} = event.currentTarget;
+
+	if (email.value === '' || password.value === '') {
+		alert('Please fill in all the fields!');
+	} else {
+		user.email = email.value;
+		user.password = password.value;
+	}
+
+	console.log(user);
+
+	event.currentTarget.reset();
+}
